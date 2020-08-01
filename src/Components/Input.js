3303ca5d050ff6907ca3fe490fec578841219484
Kedrick06 <bfloblk@gmail.com>
2020-08-01 09:50:22 -0500

    commit

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -11,18 +11,21 @@ class Input extends Component{
                 <h1>Please Login!</h1>
 
                 <form>
-                <input placeholder='Enter Your Email:' type='text'/>
+                <input placeholder='Enter Your Email:' type='text' id='submit'/>
                  <br/>
+                 <input placeholder='Enter Your Username:' type='text' id='submit'/>
+                 <br></br>
                 <input placeholder='Enter Your Password:' id='submit' type='password'/>
                 <br/>
-                <label for='female'>Female:</label>
-                 <input name='gender'id='female'  type='radio'/>
-                 <label for='male'>Male:</label>
+                <label for='female'> Female: </label>
+                 <input name='gender' id='female'  type='radio'/>
+                 <br></br>
+                 <label for='male'> Male: </label>
                 <input name='gender' id='male'type='radio'/>
                 <br/>
-                <input type='text'/>
+                <input type='text' placeholder='Enter Your Job:' name='occupation' id='submit'/>
                  <br/>
-                <input type='text'/>
+                <input type='text' placeholder='Enter Your City:' name='city' id='submit'/>
                  <br/>
                 </form>
             </div>
